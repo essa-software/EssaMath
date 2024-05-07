0d7ebadd8698fe102ddca0db299da2eeda12d0da
Adam Ratajczak <a3.ratajczak@gmail.com>
2024-05-07 16:15:32 +0200

    Change Linux Workflow

--- configure.sh
+++ configure.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd EssaMath
-sudo chmod +x init.sh
-./init.sh
-
-cd ../Tests
-sudo chmod +x init.sh
-./init.sh && cd ..
+if [ ! -d src/maxima ]
+then
+    wget -c https://sourceforge.net/projects/maxima/files/Maxima-source/5.47.0-source/maxima-5.47.0.tar.gz/download -O maxima-5.47.0.tar.gz
+    tar -xvzf maxima-5.47.0.tar.gz
+    rm maxima-5.47.0.tar.gz
+    mv maxima-5.47.0 EssaMath/src/maxima
+    cat EssaMath/src/lisp/cxx-api.lisp >> EssaMath/src/maxima/src/init-cl.lisp
+    cd EssaMath/src/maxima && ./configure --with-ecl
+    cd ../../..
+fi
